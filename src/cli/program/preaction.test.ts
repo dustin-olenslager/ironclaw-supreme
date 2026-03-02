kbarbel640-del/openclaw

fix(cli): keep json preflight stdout machine-readable

src/cli/program/preaction.test.ts
@@ -78,7 +78,18 @@ describe("registerPreActionHooks", () => {
     program.command("doctor").action(async () => {});
     program.command("completion").action(async () => {});
     program.command("secrets").action(async () => {});
-    program.command("update").action(async () => {});
+    program
+      .command("update")
+      .command("status")
+      .option("--json")
+      .action(async () => {});
+    const config = program.command("config");
+    config
+      .command("set")
+      .argument("<path>")
+      .argument("<value>")
+      .option("--json")
+      .action(async () => {});
     program.command("channels").action(async () => {});
     program.command("directory").action(async () => {});
     program.command("agents").action(async () => {});
@@ -87,6 +98,7 @@ describe("registerPreActionHooks", () => {
     program
       .command("message")
       .command("send")
+      .option("--json")
       .action(async () => {});
     registerPreActionHooks(program, "9.9.9-test");
     return program;
@@ -193,5 +205,47 @@ describe("registerPreActionHooks", () => {
 
     expect(emitCliBannerMock).not.toHaveBeenCalled();
     expect(ensureConfigReadyMock).toHaveBeenCalledTimes(1);
+  });
+
+  it("skips doctor config flow for any --json command", async () => {
+    await runCommand({
+      parseArgv: ["message", "send", "--json"],
+      processArgv: ["node", "openclaw", "message", "send", "--json"],
+    });
+
+    expect(ensureConfigReadyMock).toHaveBeenCalledWith({
+      runtime: runtimeMock,
+      commandPath: ["message", "send"],
+      jsonMode: true,
+    });
+
+    vi.clearAllMocks();
+
+    await runCommand({
+      parseArgv: ["update", "status", "--json"],
+      processArgv: ["node", "openclaw", "update", "status", "--json"],
+    });
+
+    expect(ensureConfigReadyMock).toHaveBeenCalledWith({
+      runtime: runtimeMock,
+      commandPath: ["update", "status"],
+      jsonMode: true,
+    });
+  });
+
+  it("does not treat config set --json (strict-parse alias) as json output mode", async () => {
+    await runCommand({
+      parseArgv: ["config", "set", "gateway.auth.mode", "{bad", "--json"],
+      processArgv: ["node", "openclaw", "config", "set", "gateway.auth.mode", "{bad", "--json"],
+    });
+
+    const firstCall = ensureConfigReadyMock.mock.calls[0]?.[0] as
+      | { jsonMode?: boolean }
+      | undefined;
+    expect(firstCall?.jsonMode).toBeUndefined();
+    expect(ensureConfigReadyMock).toHaveBeenCalledWith({
+      runtime: runtimeMock,
+      commandPath: ["config", "set"],
+    });
   });
 });

src/cli/route.test.ts
@@ -1,0 +1,77 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+
+const emitCliBannerMock = vi.hoisted(() => vi.fn());
+const ensureConfigReadyMock = vi.hoisted(() => vi.fn(async () => {}));
+const ensurePluginRegistryLoadedMock = vi.hoisted(() => vi.fn());
+const findRoutedCommandMock = vi.hoisted(() => vi.fn());
+const runRouteMock = vi.hoisted(() => vi.fn(async () => true));
+
+vi.mock("./banner.js", () => ({
+  emitCliBanner: emitCliBannerMock,
+}));
+
+vi.mock("./program/config-guard.js", () => ({
+  ensureConfigReady: ensureConfigReadyMock,
+}));
+
+vi.mock("./plugin-registry.js", () => ({
+  ensurePluginRegistryLoaded: ensurePluginRegistryLoadedMock,
+}));
+
+vi.mock("./program/routes.js", () => ({
+  findRoutedCommand: findRoutedCommandMock,
+}));
+
+vi.mock("../runtime.js", () => ({
+  defaultRuntime: { error: vi.fn(), log: vi.fn(), exit: vi.fn() },
+}));
+
+describe("tryRouteCli", () => {
+  let tryRouteCli: typeof import("./route.js").tryRouteCli;
+  let originalDisableRouteFirst: string | undefined;
+
+  beforeEach(async () => {
+    vi.clearAllMocks();
+    originalDisableRouteFirst = process.env.OPENCLAW_DISABLE_ROUTE_FIRST;
+    delete process.env.OPENCLAW_DISABLE_ROUTE_FIRST;
+    vi.resetModules();
+    ({ tryRouteCli } = await import("./route.js"));
+    findRoutedCommandMock.mockReturnValue({
+      loadPlugins: false,
+      run: runRouteMock,
+    });
+  });
+
+  afterEach(() => {
+    if (originalDisableRouteFirst === undefined) {
+      delete process.env.OPENCLAW_DISABLE_ROUTE_FIRST;
+    } else {
+      process.env.OPENCLAW_DISABLE_ROUTE_FIRST = originalDisableRouteFirst;
+    }
+  });
+
+  it("passes jsonMode=true for routed --json commands", async () => {
+    await expect(tryRouteCli(["node", "openclaw", "status", "--json"])).resolves.toBe(true);
+
+    expect(ensureConfigReadyMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        commandPath: ["status"],
+        jsonMode: true,
+      }),
+    );
+  });
+
+  it("does not pass jsonMode for routed non-json commands", async () => {
+    await expect(tryRouteCli(["node", "openclaw", "status"])).resolves.toBe(true);
+
+    expect(ensureConfigReadyMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        commandPath: ["status"],
+      }),
+    );
+    const firstCall = ensureConfigReadyMock.mock.calls[0]?.[0] as
+      | { jsonMode?: boolean }
+      | undefined;
+    expect(firstCall?.jsonMode).toBeUndefined();
+  });
+});

dustin-olenslager/ironclaw-supreme

src/cli/program/preaction.test.ts
@@ -78,7 +78,18 @@ describe("registerPreActionHooks", () => {
     program.command("doctor").action(async () => {});
     program.command("completion").action(async () => {});
     program.command("secrets").action(async () => {});
-    program.command("update").action(async () => {});
+    program
+      .command("update")
+      .command("status")
+      .option("--json")
+      .action(async () => {});
+    const config = program.command("config");
+    config
+      .command("set")
+      .argument("<path>")
+      .argument("<value>")
+      .option("--json")
+      .action(async () => {});
     program.command("channels").action(async () => {});
     program.command("directory").action(async () => {});
     program.command("agents").action(async () => {});
@@ -87,6 +98,7 @@ describe("registerPreActionHooks", () => {
     program
       .command("message")
       .command("send")
+      .option("--json")
       .action(async () => {});
     registerPreActionHooks(program, "9.9.9-test");
     return program;
@@ -193,5 +205,47 @@ describe("registerPreActionHooks", () => {
 
     expect(emitCliBannerMock).not.toHaveBeenCalled();
     expect(ensureConfigReadyMock).toHaveBeenCalledTimes(1);
+  });
+
+  it("suppresses doctor stdout for any --json output command", async () => {
+    await runCommand({
+      parseArgv: ["message", "send", "--json"],
+      processArgv: ["node", "openclaw", "message", "send", "--json"],
+    });
+
+    expect(ensureConfigReadyMock).toHaveBeenCalledWith({
+      runtime: runtimeMock,
+      commandPath: ["message", "send"],
+      suppressDoctorStdout: true,
+    });
+
+    vi.clearAllMocks();
+
+    await runCommand({
+      parseArgv: ["update", "status", "--json"],
+      processArgv: ["node", "openclaw", "update", "status", "--json"],
+    });
+
+    expect(ensureConfigReadyMock).toHaveBeenCalledWith({
+      runtime: runtimeMock,
+      commandPath: ["update", "status"],
+      suppressDoctorStdout: true,
+    });
+  });
+
+  it("does not treat config set --json (strict-parse alias) as json output mode", async () => {
+    await runCommand({
+      parseArgv: ["config", "set", "gateway.auth.mode", "{bad", "--json"],
+      processArgv: ["node", "openclaw", "config", "set", "gateway.auth.mode", "{bad", "--json"],
+    });
+
+    const firstCall = ensureConfigReadyMock.mock.calls[0]?.[0] as
+      | { suppressDoctorStdout?: boolean }
+      | undefined;
+    expect(firstCall?.suppressDoctorStdout).toBeUndefined();
+    expect(ensureConfigReadyMock).toHaveBeenCalledWith({
+      runtime: runtimeMock,
+      commandPath: ["config", "set"],
+    });
   });
 });

src/cli/route.test.ts
@@ -1,0 +1,77 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+
+const emitCliBannerMock = vi.hoisted(() => vi.fn());
+const ensureConfigReadyMock = vi.hoisted(() => vi.fn(async () => {}));
+const ensurePluginRegistryLoadedMock = vi.hoisted(() => vi.fn());
+const findRoutedCommandMock = vi.hoisted(() => vi.fn());
+const runRouteMock = vi.hoisted(() => vi.fn(async () => true));
+
+vi.mock("./banner.js", () => ({
+  emitCliBanner: emitCliBannerMock,
+}));
+
+vi.mock("./program/config-guard.js", () => ({
+  ensureConfigReady: ensureConfigReadyMock,
+}));
+
+vi.mock("./plugin-registry.js", () => ({
+  ensurePluginRegistryLoaded: ensurePluginRegistryLoadedMock,
+}));
+
+vi.mock("./program/routes.js", () => ({
+  findRoutedCommand: findRoutedCommandMock,
+}));
+
+vi.mock("../runtime.js", () => ({
+  defaultRuntime: { error: vi.fn(), log: vi.fn(), exit: vi.fn() },
+}));
+
+describe("tryRouteCli", () => {
+  let tryRouteCli: typeof import("./route.js").tryRouteCli;
+  let originalDisableRouteFirst: string | undefined;
+
+  beforeEach(async () => {
+    vi.clearAllMocks();
+    originalDisableRouteFirst = process.env.OPENCLAW_DISABLE_ROUTE_FIRST;
+    delete process.env.OPENCLAW_DISABLE_ROUTE_FIRST;
+    vi.resetModules();
+    ({ tryRouteCli } = await import("./route.js"));
+    findRoutedCommandMock.mockReturnValue({
+      loadPlugins: false,
+      run: runRouteMock,
+    });
+  });
+
+  afterEach(() => {
+    if (originalDisableRouteFirst === undefined) {
+      delete process.env.OPENCLAW_DISABLE_ROUTE_FIRST;
+    } else {
+      process.env.OPENCLAW_DISABLE_ROUTE_FIRST = originalDisableRouteFirst;
+    }
+  });
+
+  it("passes suppressDoctorStdout=true for routed --json commands", async () => {
+    await expect(tryRouteCli(["node", "openclaw", "status", "--json"])).resolves.toBe(true);
+
+    expect(ensureConfigReadyMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        commandPath: ["status"],
+        suppressDoctorStdout: true,
+      }),
+    );
+  });
+
+  it("does not pass suppressDoctorStdout for routed non-json commands", async () => {
+    await expect(tryRouteCli(["node", "openclaw", "status"])).resolves.toBe(true);
+
+    expect(ensureConfigReadyMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        commandPath: ["status"],
+      }),
+    );
+    const firstCall = ensureConfigReadyMock.mock.calls[0]?.[0] as
+      | { suppressDoctorStdout?: boolean }
+      | undefined;
+    expect(firstCall?.suppressDoctorStdout).toBeUndefined();
+  });
+});